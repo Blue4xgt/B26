282f89847ed87c5f489add2adeee3fa17f9a9455
Blue4xgt <datsungocommunityind@gmail.com>
2022-05-03 05:12:09 +0700

    B26

---
+++ admin/iklan.js
@@ -0,0 +1,11 @@
+exports.iklan1 = () => {
+  return `[ PeopleSMP: https://chat.whatsapp.com/Gj4xK2DFxDUEJ5xRBwZM2f ]`
+};
+
+exports.iklan2 = () => {
+  return `[ YOUR_IKLAN ]`
+};
+
+exports.iklan3 = () => {
+  return `[ YOUR_IKLAN ]`
+};
